91f5f84b7d8fc3a49b9d7e8f256a6ab7fbdc7a53
Matt Weathers <matt@getnacelle.com>
2021-08-30 15:41:04 -0500

    Show all services for ps. Colorize and format ps

--- utils.js
+++ utils.js
@@ -1,8 +1,11 @@
-const os = require('os');
 const fs = require('fs');
 const yaml = require('js-yaml');
+const merge = require('lodash.merge');
+const columnify = require('columnify');
+const chalk = require('chalk');
+const sortBy = require('lodash.sortby');
 
-const STATE_PATH = `${os.homedir()}/.docker-maestro.state`;
+const STATE_PATH = `${process.cwd()}/.docker-maestro.state`;
 const MAESTRO_PATH = `./docker-maestro.yml`;
 
 const getState = () => {
@@ -15,10 +18,20 @@ const getState = () => {
 
 const showState = () => {
     if (fs.existsSync(STATE_PATH)) {
+        const services = yaml.safeLoad(fs.readFileSync(MAESTRO_PATH)) || {};
         const state = yaml.safeLoad(fs.readFileSync(STATE_PATH)) || {};
-        Object.entries(state).map(([key, value]) => {
-            console.log(`${key}\t${value}`);
+
+        const fullState = merge(
+          merge(...Object.keys(services).map((key) => ({ [key]: 'use-image' }))),
+          merge(...Object.entries(state).map(([key, value]) => ({ [key]: value })))
+        );
+
+        const mappedState = Object.entries(fullState).map(([key, value]) => {
+          const config = value === 'use-local' ? chalk.green(value) : value;
+          return { service: key, config };
         });
+
+        console.log(columnify(sortBy(mappedState, 'service'), { minWidth: 30 }));
     }
 };
 
@@ -32,7 +45,7 @@ const writeState = (state) => {
 
 const getServices = () => {
     if (fs.existsSync(MAESTRO_PATH)) {
-        return yaml.safeLoad(fs.readFileSync('./docker-maestro.yml'));
+        return yaml.safeLoad(fs.readFileSync(`${MAESTRO_PATH}`));
     }
     console.log('No docker-maestro.yml found.');
     process.exit(1);
